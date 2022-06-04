dd8f1caa78752ddaa494c591020967345ca233e6
SantiagoGuiral <santiagowslg@gmail.com>
2022-06-04 11:24:02 -0500

    update login component

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,7 +1,5 @@
 import React from 'react'
-import Logo from "../resources/images/Spotify_logo.png"
-
-
+import Logo from "../resources/images/logo.png"
 
 const AUTH_URL = "https://accounts.spotify.com/authorize?client_id=78e9a4af26e24576825ad5914863a087&response_type=code&redirect_uri=http://localhost:3000&scope=streaming%20user-read-email%20user-read-private%20user-library-read%20user-library-modify%20user-read-playback-state%20user-modify-playback-state"
 
@@ -9,7 +7,7 @@ const Login = () => {
   return (
     <div className='login-page'>
         <img className='login-img' src={Logo} alt="spotify_logo"/>
-        <h1 className='login-title'>Spotify Clone</h1>
+        <h1 className='login-title'>My Music App</h1>
         <a className='login-button' href={AUTH_URL}>LOGIN</a>
     </div>
   )
