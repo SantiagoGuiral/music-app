345b8aae05d4ce0aef30a8f6f9f9d84f04dc2ca7
SantiagoGuiral <santiagowslg@gmail.com>
2022-06-05 13:21:08 -0500

    login view

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,5 +1,4 @@
-import React from 'react'
-import Logo from "../resources/images/logo.png"
+import Logo from "../assets/icons/spotify-logo.svg"
 
 const AUTH_URL = "https://accounts.spotify.com/authorize?client_id=78e9a4af26e24576825ad5914863a087&response_type=code&redirect_uri=http://localhost:3000&scope=streaming%20user-read-email%20user-read-private%20user-library-read%20user-library-modify%20user-read-playback-state%20user-modify-playback-state"
 
